7a97cff4731822b20f7ef75409eef868d31e7589
Caleb Mustapha <cmustapha@academic.rrc.ca>
2025-03-27 10:53:49 -0500

    Configure ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -6,9 +6,13 @@ import pluginReact from "eslint-plugin-react";
 
 
 export default defineConfig([
+  { settings: { react: { version: 'detect'} } },
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
+
+  // "recommended rules for code correctness that you can drop in without additional configuration"
+  // https://typescript-eslint.io/users/configs/#recommended
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
 ]);
